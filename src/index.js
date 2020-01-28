18c1b86058a6577777edeb62bcccf98fa7b6a75d
faizhameed <faizftb@gmail.com>
2020-01-28 17:53:56 +0530

    added graphql to fetch collections overview

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient, gql } from "apollo-boost"; // bundle of small libraries,
+import { store, persistor } from "./redux/store";
 
-import { store, persistor } from './redux/store';
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com"
+});
+//create our cache
 
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+  <div className="shop-page">
+    <Route
+      exact
+      path={`${match.path}`}
+      component={CollectionsOverviewContainer}
+    />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
