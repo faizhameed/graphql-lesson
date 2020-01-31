e8fdc28b8b1a9cb7ce9fe999053667519b1fa62c
faizhameed <faizftb@gmail.com>
2020-01-31 10:17:04 +0530

    Graphql apollo container for collection page added

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
+import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
@@ -21,8 +18,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
